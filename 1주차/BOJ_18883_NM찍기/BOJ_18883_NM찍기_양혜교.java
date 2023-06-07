59f8a4bd8b60ebf4d8d81e52500af2f9ff4ba3fa
YangHyeGyo <gpry4618@naver.com>
2023-06-07 20:28:55 +0900

    [1주차] 양혜교

--- 1주차/BOJ_18883_NM찍기/BOJ_18883_NM찍기_양혜교.java
+++ 1주차/BOJ_18883_NM찍기/BOJ_18883_NM찍기_양혜교.java
@@ -1,28 +1,26 @@
 package BOJ_18883_NM찍기;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class BOJ_18883_NM찍기_양혜교 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] strArr = br.readLine().split(" ");
+        int N = Integer.parseInt(strArr[0]);
+        int M = Integer.parseInt(strArr[1]);
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        int N = scan.nextInt();
-        int M = scan.nextInt();
-
-        int[] arr = new int[N*M];
-        int a = 0;
-
-        for(int i=0; i<N*M; i++){
-            a+=1;
-            arr[i] = a;
-        }
-
-        for(int i=0; i<N*M; i++){
-            System.out.print(arr[i] + " ");
-            if(arr[i]%M == 0){
-                System.out.println();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 1; i <= N * M; i++) {
+            bw.write(Integer.toString(i));
+            if (i % M == 0) {
+                bw.write("\n");
+            }else{
+                bw.write(" ");
             }
         }
+        bw.close();
+
     }
+
 }
